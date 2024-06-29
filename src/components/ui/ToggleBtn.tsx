5b365669407b1fa29870ccbb4ba6d9db177f0198
Crazy0717 <bpcrazy28@gmail.com>
2024-06-29 13:26:04 +0500

    v1.035
    fixed ToggleBtn

--- src/components/ui/ToggleBtn.tsx
+++ src/components/ui/ToggleBtn.tsx
@@ -31,13 +31,10 @@ const ToggleBtn = () => {
 
   return (
     <div className="inline-block ">
-      <div
-        onClick={toggleSwitch}
-        className="w-[2em] h-[1.2em] absolute rigth-0 z-[1] rounded-[1em]"
-      ></div>
       <label className="switch w-[2em] h-[1em] inline-block bg-light-1 text-[20px] rounded-[1em] dark:bg-dark-2">
         <input
-          defaultChecked={isDarkMode}
+          checked={isDarkMode}
+          onChange={toggleSwitch}
           className="absolute opacity-0"
           type="checkbox"
         />
